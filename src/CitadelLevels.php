fc244396afa94621186eb4e19fa1490ab0f2113e
Oscar Ellis <oscarx1be@gmail.com>
2025-04-24 13:25:45 +0100

    plugin level files

---
+++ src/CitadelLevels.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Citadel\Levels;
+
+
+use Forumify\Plugin\AbstractForumifyPlugin;
+use Forumify\Plugin\PluginMetadata;
+
+
+class CitadelLevels extends AbstractForumifyPlugin
+{
+    public function getPluginMetadata(): PluginMetadata
+    {
+        return new PluginMetadata(
+            'Levels',
+            'Citadel Software Solutions',
+        );
+    }
+
+    public function getPermissions(): array
+    {
+        return [
+            'admin' => [ 'view' ]
+        ];
+    }
+}
