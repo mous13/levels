f9806ffeefc26a9c72210ac49b6034f08f367ca8
Oscar Ellis <oscarx1be@gmail.com>
2025-04-25 22:22:28 +0100

    added twig extension to check if config is set

--- src/Core/Twig/LevelExtension.php
+++ src/Core/Twig/LevelExtension.php
@@ -6,6 +6,7 @@ use Citadel\Levels\Core\Entity\Level;
 use Citadel\Levels\Core\Repository\LevelRepository;
 use Citadel\Levels\Core\Repository\UserXpRepository;
 use Forumify\Core\Entity\User;
+use Forumify\Core\Repository\SettingRepository;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -13,17 +14,24 @@ class LevelExtension extends AbstractExtension
 {
     public function __construct(
         private readonly LevelRepository $levels,
-        private UserXpRepository $userXpRepository
+        private readonly UserXpRepository $userXpRepository,
+        private readonly SettingRepository $settingRepository,
     ){}
 
     public function getFunctions()
     {
         return [
+            new TwigFunction('levelConfig', [$this, 'getLevelConfig']),
             new TwigFunction('level', [$this, 'getUserLevel']),
             new TwigFunction('xp', [$this, 'getUserXp']),
         ];
     }
-
+    public function getLevelConfig(): bool
+    {
+        $threadXp = $this->settingRepository->get('levels.thread_post_xp');
+        $postXp = $this->settingRepository->get('levels.comment_post_xp');
+        return $threadXp !== null && $postXp !== null;
+    }
     public function getUserLevel(User $user): ?Level
     {
         $xp = $this->getUserXp($user);
